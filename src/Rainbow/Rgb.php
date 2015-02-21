be10b9441bc63a9b03012976238ae526bb911f36
rober <rmh.dev@gmail.com>
2015-02-21 18:53:59 +0100

    Configuring phpunit

---
+++ src/Rainbow/Rgb.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of the Rainbow package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
+namespace Rainbow;
+
+class Rgb
+{
+    public function getRed()
+    {
+        return 0;
+    }
+}
