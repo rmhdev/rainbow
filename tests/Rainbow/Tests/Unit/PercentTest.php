19b7249c1d1ec206bcc488d935871d366f8c38d4
rober <rmh.dev@gmail.com>
2015-02-22 13:33:42 +0100

    Include tests

---
+++ tests/Rainbow/Tests/Unit/PercentTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Rainbow\Tests\Unit;
+
+use Rainbow\Unit\Percent;
+
+class PercentTest extends \PHPUnit_Framework_TestCase
+{
+    public function testEmptyValueShouldReturnZero()
+    {
+        $unit = new Percent();
+
+        $this->assertEquals(0, $unit->getValue());
+        $this->assertInternalType("int", $unit->getValue());
+    }
+
+    /**
+     * @dataProvider getCorrectValuesDataProvider
+     */
+    public function testCorrectValueShouldReturnValue($value, $expectedValue)
+    {
+        $unit = new Percent($value);
+
+        $this->assertEquals($expectedValue, $unit->getValue());
+        $this->assertInternalType("int", $unit->getValue());
+    }
+
+    public function getCorrectValuesDataProvider()
+    {
+        return array(
+            array(20, 20),
+            array("20", 20),
+            array("20%", 20),
+        );
+    }
+}
