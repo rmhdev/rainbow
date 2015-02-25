ec5bb92194397cd453e80a47822178d348c95254
rober <rmh.dev@gmail.com>
2015-02-25 23:21:15 +0100

    Angle implements UnitInterface

--- src/Rainbow/Unit/Angle.php
+++ src/Rainbow/Unit/Angle.php
@@ -2,10 +2,13 @@
 
 namespace Rainbow\Unit;
 
-final class Angle
+final class Angle implements UnitInterface
 {
     private $value;
 
+    /**
+     * @param int|string $value
+     */
     public function __construct($value = 0)
     {
         $number = $this->toNumber($value);
@@ -21,11 +24,17 @@ final class Angle
         return intval($value, 10);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getValue()
     {
         return $this->value;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function __toString()
     {
         return (string) $this->getValue();
