295374df1e03a4cdc2750792488ffac456b4d247
rober <rmh.dev@gmail.com>
2015-03-06 22:44:22 +0100

    Improve phpdocs

--- src/Rainbow/Translator/Translator.php
+++ src/Rainbow/Translator/Translator.php
@@ -1,19 +1,35 @@
 <?php
 
+/**
+ * This file is part of the Rainbow package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
 namespace Rainbow\Translator;
 
 use Rainbow\ColorInterface;
 use Rainbow\Converter\ConverterFactory;
+use Rainbow\Rgb;
+use Rainbow\Hsl;
 
 final class Translator
 {
     private $color;
 
+    /**
+     * @param ColorInterface $color  A color to translate
+     */
     public function __construct(ColorInterface $color)
     {
         $this->color = $color;
     }
 
+    /**
+     * @return Rgb
+     */
     public function toRgb()
     {
         return ConverterFactory::create($this->getColor(), "rgb")->convert();
@@ -24,6 +40,9 @@ final class Translator
         return $this->color;
     }
 
+    /**
+     * @return Hsl
+     */
     public function toHsl()
     {
         return ConverterFactory::create($this->getColor(), "hsl")->convert();
