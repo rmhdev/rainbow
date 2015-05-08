4c475560a44c7494a9cbae20f528606ee8343978
rober <rmh.dev@gmail.com>
2015-05-08 21:35:33 +0200

    Now the contrast class accepts all kind of colors

--- src/Rainbow/Calculation/Operation/Contrast.php
+++ src/Rainbow/Calculation/Operation/Contrast.php
@@ -3,19 +3,17 @@
 namespace Rainbow\Calculation\Operation;
 
 use Rainbow\Calculation\CalculationInterface;
-use Rainbow\Rgb;
 use Rainbow\ColorInterface;
-
 final class Contrast implements CalculationInterface
 {
     private $contrast;
 
-    public function __construct(Rgb $color, Rgb $dark, Rgb $light)
+    public function __construct(ColorInterface $color, ColorInterface $dark, ColorInterface $light)
     {
         $this->contrast = $this->calculateContrast($color, $dark, $light);
     }
 
-    private function calculateContrast(Rgb $color, Rgb $dark, Rgb $light)
+    private function calculateContrast(ColorInterface $color, ColorInterface $dark, ColorInterface $light)
     {
         $colorLuma = $color->luma()->getValue();
         $darkLuma = $dark->luma()->getValue();
