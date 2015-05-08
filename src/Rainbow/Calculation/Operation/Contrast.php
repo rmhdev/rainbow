7c39419e361c284e8a04f65efc5afb555707d3ad
rober <rmh.dev@gmail.com>
2015-05-08 20:55:31 +0200

    Return white color contrasting a dark color

--- src/Rainbow/Calculation/Operation/Contrast.php
+++ src/Rainbow/Calculation/Operation/Contrast.php
@@ -7,9 +7,21 @@ use Rainbow\Rgb;
 
 final class Contrast implements CalculationInterface
 {
+    private $contrast;
+
+    public function __construct(Rgb $color)
+    {
+        $this->contrast = $this->calculateContrast($color);
+    }
+
+    private function calculateContrast(Rgb $color)
+    {
+        return $color->getRed()->getValue() ? new Rgb(0, 0, 0) : new Rgb(255, 255, 255);
+    }
+
     public function result()
     {
-        return new Rgb();
+        return $this->contrast->copy();
     }
 
 }
