6c25c136ee74ca676bc24fcfefbf4d0100874e73
rober <rmh.dev@gmail.com>
2015-05-08 09:04:53 +0200

    Calculate luminance

--- src/Rainbow/Calculation/Luminance.php
+++ src/Rainbow/Calculation/Luminance.php
@@ -2,16 +2,32 @@
 
 namespace Rainbow\Calculation;
 
-use Rainbow\Hsl;
+use Rainbow\Rgb;
+use Rainbow\Unit\Component;
 use Rainbow\Unit\Percent;
 
 final class Luminance
 {
     private $value;
 
-    public function __construct(Hsl $color)
+    public function __construct(Rgb $color)
     {
-        $this->value = $color->getSaturation()->getValue() ? 100 : 0;
+        $this->value = $this->calculateValue($color);
+    }
+
+    private function calculateValue(Rgb $color)
+    {
+        $red = $color->getRed()->getValue() / Component::MAX_VALUE;
+        $green = $color->getGreen()->getValue() / Component::MAX_VALUE;
+        $blue = $color->getBlue()->getValue() / Component::MAX_VALUE;
+
+        $red    = ($red <= 0.03928)     ? $red / 12.92 : (($red + 0.055) / 1.055) ** 2.4;
+        $green  = ($green <= 0.03928)   ? $green / 12.92 : (($green + 0.055) / 1.055) ** 2.4;
+        $blue   = ($blue <= 0.03928)    ? $blue / 12.92 : (($blue + 0.055) / 1.055) ** 2.4;
+
+        $value = 0.2126 * $red + 0.7152 * $green + 0.0722 * $blue;
+
+        return $value * 100;
     }
 
     public function value()
--- tests/Rainbow/Tests/Calculation/LuminanceTest.php
+++ tests/Rainbow/Tests/Calculation/LuminanceTest.php
@@ -3,14 +3,14 @@
 namespace Rainbow\Tests\Calculation;
 
 use Rainbow\Calculation\Luminance;
-use Rainbow\Hsl;
+use Rainbow\Rgb;
 use Rainbow\Unit\Percent;
 
 class LuminanceTest extends \PHPUnit_Framework_TestCase
 {
     public function testLuminanceInBlackShouldReturnMinValue()
     {
-        $color = new Hsl(0, 0, 0);
+        $color = new Rgb(0, 0, 0);
         $luminance = new Luminance($color);
 
         $this->assertEquals(new Percent(0), $luminance->value());
@@ -18,9 +18,28 @@ class LuminanceTest extends \PHPUnit_Framework_TestCase
 
     public function testLuminanceInWhiteShouldReturnMaxValue()
     {
-        $color = new Hsl(360, 100, 100);
+        $color = new Rgb(255, 255, 255);
         $luminance = new Luminance($color);
 
         $this->assertEquals(new Percent(100), $luminance->value());
+    }
+
+    /**
+     * @dataProvider getLuminanceDataProvider
+     */
+    public function testLuminanceShouldReturnValidPercentage($expected, $value)
+    {
+        $color = new Rgb($value["red"], $value["green"], $value["blue"]);
+        $luminance = new Luminance($color);
+
+        $this->assertEquals(new Percent($expected), $luminance->value());
+    }
+
+    public function getLuminanceDataProvider()
+    {
+        return array(
+            array(13, array("red" => 100, "green" => 100, "blue" => 100)),
+            array(35, array("red" => 160, "green" => 160, "blue" => 160)),
+        );
     }
 }
