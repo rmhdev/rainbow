05e77ad3b92fcae7889a7f17d5c59b340e3e1dae
rober <rmh.dev@gmail.com>
2015-05-08 10:20:30 +0200

    Use luminance class in colors

--- src/Rainbow/AbstractColor.php
+++ src/Rainbow/AbstractColor.php
@@ -10,10 +10,9 @@
 
 namespace Rainbow;
 
+use Rainbow\Calculation\Luminance;
 use Rainbow\Calculation\Operation\Saturation;
 use Rainbow\Translator\Translator;
-use Rainbow\Unit\Component;
-use Rainbow\Unit\Percent;
 
 abstract class AbstractColor implements ColorInterface
 {
@@ -155,17 +154,8 @@ abstract class AbstractColor implements ColorInterface
      */
     public function luminance()
     {
-        $rgb = $this->getLocalHsl()->translate()->toRgb();
-        $red = $rgb->getRed()->getValue() / Component::MAX_VALUE;
-        $green = $rgb->getGreen()->getValue() / Component::MAX_VALUE;
-        $blue = $rgb->getBlue()->getValue() / Component::MAX_VALUE;
+        $luminance = new Luminance($this->translate()->toRgb());
 
-        $red    = ($red <= 0.03928)     ? $red / 12.92 : (($red + 0.055) / 1.055) ** 2.4;
-        $green  = ($green <= 0.03928)   ? $green / 12.92 : (($green + 0.055) / 1.055) ** 2.4;
-        $blue   = ($blue <= 0.03928)    ? $blue / 12.92 : (($blue + 0.055) / 1.055) ** 2.4;
-
-        $value = 0.2126 * $red + 0.7152 * $green + 0.0722 * $blue;
-
-        return new Percent($value * 100);
+        return $luminance->getValue();
     }
 }
