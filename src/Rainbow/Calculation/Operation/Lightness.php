8404029370a72d78d8393bc7a81beca2dec9cfc0
rober <rmh.dev@gmail.com>
2015-05-08 11:48:17 +0200

    Calculate lightness in a new class

---
+++ src/Rainbow/Calculation/Operation/Lightness.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of the Rainbow package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
+namespace Rainbow\Calculation\Operation;
+
+use Rainbow\Hsl;
+use Rainbow\Unit\Percent;
+
+final class Lightness
+{
+    private $lightness;
+
+    /**
+     * @param Hsl $color
+     * @param int $difference
+     */
+    public function __construct(Hsl $color, $difference = 0)
+    {
+        $this->lightness = $this->calculateNewLightness(
+            $color->getLightness()->getValue(),
+            $difference
+        );
+    }
+
+    private function calculateNewLightness($value, $difference)
+    {
+        if (!$difference) {
+            return $value;
+        }
+        if ($difference < 0) {
+            return max($value + $difference, 0);
+        }
+
+        return min($value + $difference, 100);
+    }
+
+    /**
+     * @return Percent
+     */
+    public function result()
+    {
+        return new Percent($this->lightness);
+    }
+}
---
+++ tests/Rainbow/Tests/Calculation/Operation/LightnessTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Rainbow\Tests\Calculation\Operation;
+
+use Rainbow\Calculation\Operation\Lightness;
+use Rainbow\Hsl;
+use Rainbow\Unit\Percent;
+
+class LightnessTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLightnessShouldReturnUpdatedPercent()
+    {
+        $color = new Hsl(180, 50, 0);
+        $operation = new Lightness($color, 10);
+        $lightness = $operation->result();
+
+        $this->assertEquals(new Percent(10), $lightness);
+    }
+
+    public function testLightnessShouldBeLesserEqualThan100()
+    {
+        $color = new Hsl(180, 50, 80);
+        $operation = new Lightness($color, 30);
+
+        $this->assertEquals(new Percent(100), $operation->result());
+    }
+
+    public function testLightnessShouldBeGreaterEqualThanZero()
+    {
+        $color = new Hsl(180, 50, 20);
+        $operation = new Lightness($color, -30);
+
+        $this->assertEquals(new Percent(0), $operation->result());
+    }
+
+    public function testEmptyDifferenceShouldReturnEqualColor()
+    {
+        $color = new Hsl(180, 50, 20);
+        $operation = new Lightness($color);
+
+        $this->assertEquals(new Percent(20), $operation->result());
+    }
+}
