604ca7da32a43e286933e2bba979a3cc2709f161
rober <rmh.dev@gmail.com>
2015-05-10 12:48:54 +0200

    Test luminance in white color

--- src/Rainbow/Calculation/Channel/Luminance.php
+++ src/Rainbow/Calculation/Channel/Luminance.php
@@ -2,12 +2,25 @@
 
 namespace Rainbow\Calculation\Channel;
 
+use Rainbow\Rgb;
 use Rainbow\Unit\Percent;
 
 final class Luminance
 {
+    private $value;
+
+    public function __construct(Rgb $color)
+    {
+        $this->value = $this->calculateLuminance($color);
+    }
+
+    private function calculateLuminance(Rgb $color)
+    {
+        return $color->getRed()->getValue() ? 100 : 0;
+    }
+
     public function result()
     {
-        return new Percent(0);
+        return new Percent($this->value);
     }
 }
--- tests/Rainbow/Tests/Calculation/Channel/LuminanceTest.php
+++ tests/Rainbow/Tests/Calculation/Channel/LuminanceTest.php
@@ -11,8 +11,16 @@ class LuminanceTest extends \PHPUnit_Framework_TestCase
     public function testBlackShouldReturnMinValue()
     {
         $color = new Rgb(0, 0, 0);
-        $luminance = new Luminance($color);
+        $operation = new Luminance($color);
 
-        $this->assertEquals(new Percent(0), $luminance->result());
+        $this->assertEquals(new Percent(0), $operation->result());
+    }
+
+    public function testWhiteShouldReturnMinValue()
+    {
+        $color = new Rgb(255, 255, 255);
+        $operation = new Luminance($color);
+
+        $this->assertEquals(new Percent(100), $operation->result());
     }
 }
