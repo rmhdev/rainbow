9df49ef0b5be2b7e682b7ff6e9d6509041b2c790
rober <rmh.dev@gmail.com>
2015-05-12 11:04:50 +0200

    First test with overlay

---
+++ src/Rainbow/Calculation/Blending/overlay.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the Rainbow package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
+namespace Rainbow\Calculation\Blending;
+
+use Rainbow\Rgb;
+
+class Overlay
+{
+    public function result()
+    {
+        return new Rgb(255, 255, 255);
+    }
+}
---
+++ tests/Rainbow/Tests/Calculation/Blending/OverlayTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the Rainbow package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
+namespace Rainbow\Tests\Calculation\Blending;
+
+use Rainbow\Calculation\Blending\Overlay;
+use Rainbow\Rgb;
+
+class OverlayTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWhiteWithBlackShouldReturnWhite()
+    {
+        $white = new Rgb(255, 255, 255);
+        $black = new Rgb(0, 0, 0);
+        $operation = new Overlay($white, $black);
+
+        $this->assertEquals($white, $operation->result());
+    }
+}
