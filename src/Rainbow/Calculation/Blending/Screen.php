5f5d3345d9eaabbdc2cbe387c8dec9643d69f890
rober <rmh.dev@gmail.com>
2015-05-12 09:39:23 +0200

    Screening with white results in white

--- src/Rainbow/Calculation/Blending/Screen.php
+++ src/Rainbow/Calculation/Blending/Screen.php
@@ -1,13 +1,28 @@
 <?php
 
+/**
+ * This file is part of the Rainbow package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
 namespace Rainbow\Calculation\Blending;
 
 use Rainbow\Rgb;
 
 final class Screen
 {
+    private $result;
+
+    public function __construct(Rgb $color1, Rgb $color2)
+    {
+        $this->result = $color2->getRed()->getValue() ? $color2 : $color1;
+    }
+
     public function result()
     {
-        return new Rgb(255, 102, 0);
+        return $this->result->copy();
     }
 }
--- tests/Rainbow/Tests/Calculation/Blending/ScreenTest.php
+++ tests/Rainbow/Tests/Calculation/Blending/ScreenTest.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the Rainbow package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
 namespace Rainbow\Tests\Calculation\Blending;
 
 use Rainbow\Calculation\Blending\Screen;
@@ -14,5 +22,14 @@ class ScreenTest extends \PHPUnit_Framework_TestCase
         $operation = new Screen($color, $black);
 
         $this->assertEquals($color, $operation->result());
+    }
+
+    public function testScreenWithWhiteShouldReturnWhite()
+    {
+        $color = new Rgb(255, 102, 0);
+        $white = new Rgb(255, 255, 255);
+        $operation = new Screen($color, $white);
+
+        $this->assertEquals($white, $operation->result());
     }
 }
