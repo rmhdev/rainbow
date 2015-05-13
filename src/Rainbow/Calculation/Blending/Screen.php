f65f6a4a7fce3233466e90c8978bc1dd77ffe41f
rober <rmh.dev@gmail.com>
2015-05-13 17:17:02 +0200

    Simplify component calc

--- src/Rainbow/Calculation/Blending/Screen.php
+++ src/Rainbow/Calculation/Blending/Screen.php
@@ -12,6 +12,7 @@ namespace Rainbow\Calculation\Blending;
 
 use Rainbow\Calculation\CalculationInterface;
 use Rainbow\Rgb;
+use Rainbow\Unit\RgbComponent;
 
 final class Screen implements CalculationInterface
 {
@@ -26,17 +27,23 @@ final class Screen implements CalculationInterface
      */
     public function __construct(Rgb $color1, Rgb $color2)
     {
-        $redSum     = $color1->getRed()->getValue() + $color2->getRed()->getValue() ;
-        $greenSum   = $color1->getGreen()->getValue() + $color2->getGreen()->getValue();
-        $blueSum    = $color1->getBlue()->getValue() + $color2->getBlue()->getValue();
-        $multiply   = new Multiply($color1, $color2);
-        $multiplied = $multiply->result();
+        $red = $this->calculateComponentValue($color1->getRed(), $color2->getRed());
+        $green = $this->calculateComponentValue($color1->getGreen(), $color2->getGreen());
+        $blue = $this->calculateComponentValue($color1->getBlue(), $color2->getBlue());
 
-        $this->result = new Rgb(
-            abs($redSum - $multiplied->getRed()->getValue()),
-            abs($greenSum - $multiplied->getGreen()->getValue()),
-            abs($blueSum - $multiplied->getBlue()->getValue())
-        );
+        $this->result = new Rgb($red, $green, $blue);
+    }
+
+
+
+    private function calculateComponentValue(RgbComponent $component1, RgbComponent $component2)
+    {
+        return $component1->getValue() + $component2->getValue() - ($this->multiply($component1, $component2));
+    }
+
+    protected function multiply(RgbComponent $component1, RgbComponent $component2)
+    {
+        return $component1->getValue() * $component2->getValue() / RgbComponent::MAX_VALUE;
     }
 
     /**
