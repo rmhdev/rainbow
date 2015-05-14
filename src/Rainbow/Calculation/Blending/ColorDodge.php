6ec7bd60bf8fda5bf76e2bed7417749e7b6696f9
rober <rmh.dev@gmail.com>
2015-05-14 16:29:23 +0200

    Complete color dodge blending class

--- src/Rainbow/Calculation/Blending/ColorDodge.php
+++ src/Rainbow/Calculation/Blending/ColorDodge.php
@@ -1,26 +1,39 @@
 <?php
 
+/**
+ * This file is part of the Rainbow package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
 namespace Rainbow\Calculation\Blending;
 
 use Rainbow\Calculation\CalculationInterface;
 use Rainbow\Unit\RgbComponent;
 
+/**
+ * Brightens the backdrop color to reflect the source color
+ * @package Rainbow\Calculation\Blending
+ * @link http://www.w3.org/TR/compositing-1/#valdef-blend-mode-color-dodge
+ */
 final class ColorDodge extends AbstractBlending implements CalculationInterface
 {
     /**
-     * @param RgbComponent $component1
-     * @param RgbComponent $component2
-     * @return int
+     * {@inheritDoc}
      */
     protected function calculateComponentValue(RgbComponent $component1, RgbComponent $component2)
     {
-        if ($component1->getValue() == 0) {
+        $value1 = $component1->getValue() / RgbComponent::MAX_VALUE;
+        $value2 = $component2->getValue() / RgbComponent::MAX_VALUE;
+        if ($value1 == 0) {
             return 0;
         }
-        if ($component2->getValue() == RgbComponent::MAX_VALUE) {
+        if ($value2 == 1) {
             return RgbComponent::MAX_VALUE;
         }
 
-        return 500;
+        return min(1, $value1 / (1 - $value2)) * RgbComponent::MAX_VALUE;
     }
 }
--- tests/Rainbow/Tests/Calculation/Blending/ColorDodgeTest.php
+++ tests/Rainbow/Tests/Calculation/Blending/ColorDodgeTest.php
@@ -30,4 +30,32 @@ class ColorDodgeTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($white, $blend->result());
     }
+
+    /**
+     * @dataProvider blendingDataProvider
+     * @param Rgb $expected
+     * @param Rgb $color
+     */
+    public function testColorShouldReturnCorrectColor(Rgb $expected, Rgb $color)
+    {
+        $baseColor = new Rgb(255, 102, 0);
+        $overlay = new ColorDodge($baseColor, $color);
+
+        $this->assertEquals($expected, $overlay->result());
+    }
+
+    public function blendingDataProvider()
+    {
+        return array(
+            array(new Rgb(255, 102, 0), new Rgb(0, 0, 0)),
+            array(new Rgb(255, 128, 0), new Rgb(51, 51, 51)),
+            array(new Rgb(255, 170, 0), new Rgb(102, 102, 102)),
+            array(new Rgb(255, 255, 0), new Rgb(153, 153, 153)),
+            array(new Rgb(255, 255, 0), new Rgb(204, 204, 204)),
+            array(new Rgb(255, 255, 0), new Rgb(255, 255, 255)),
+            array(new Rgb(255, 102, 0), new Rgb(255, 0, 0)),
+            array(new Rgb(255, 255, 0), new Rgb(0, 255, 0)),
+            array(new Rgb(255, 102, 0), new Rgb(0, 0, 255)),
+        );
+    }
 }
