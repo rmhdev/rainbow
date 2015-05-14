e52c2d2a33458713f9e483d6c14aaae1cd5fdf08
rober <rmh.dev@gmail.com>
2015-05-14 10:48:35 +0200

    Calculate the hardlight value

---
+++ src/Rainbow/Calculation/Blending/HardLight.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Rainbow\Calculation\Blending;
+
+use Rainbow\Calculation\CalculationInterface;
+use Rainbow\Rgb;
+
+/**
+ * Class HardLight
+ * @package Rainbow\Calculation\Blending
+ * @link http://www.w3.org/TR/compositing-1/#valdef-blend-mode-hard-light
+ */
+final class HardLight implements CalculationInterface
+{
+    /**
+     * @var Rgb
+     */
+    private $color;
+
+    /**
+     * @param Rgb $color1
+     * @param Rgb $color2
+     */
+    public function __construct(Rgb $color1, Rgb $color2)
+    {
+        $overlay = new Overlay($color2, $color1);
+        $this->color = $overlay->result();
+    }
+
+    /**
+     * @return Rgb
+     */
+    public function result()
+    {
+        return $this->color->copy();
+    }
+}
---
+++ tests/Rainbow/Tests/Calculation/Blending/HardLightTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Rainbow\Tests\Calculation\Blending;
+
+use Rainbow\Calculation\Blending\HardLight;
+use Rainbow\Rgb;
+
+class HardLightTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWhiteWithBlackShouldReturnBlack()
+    {
+        $white = new Rgb(255, 255, 255);
+        $black = new Rgb(0, 0, 0);
+        $operation = new HardLight($white, $black);
+
+        $this->assertEquals($black, $operation->result());
+    }
+
+    public function testBlackWithWhiteShouldReturnWhite()
+    {
+        $white = new Rgb(255, 255, 255);
+        $black = new Rgb(0, 0, 0);
+        $operation = new HardLight($black, $white);
+
+        $this->assertEquals($white, $operation->result());
+    }
+
+    /**
+     * @dataProvider colorDataProvider
+     * @param Rgb $expected
+     * @param Rgb $color
+     */
+    public function testColorShouldReturnCorrectColor(Rgb $expected, Rgb $color)
+    {
+        $baseColor = new Rgb(255, 102, 0);
+        $overlay = new HardLight($baseColor, $color);
+
+        $this->assertEquals($expected, $overlay->result());
+    }
+
+    public function colorDataProvider()
+    {
+        return array(
+            array(new Rgb(0, 0, 0), new Rgb(0, 0, 0)),
+            array(new Rgb(102, 41, 0), new Rgb(51, 51, 51)),
+            array(new Rgb(204, 82, 0), new Rgb(102, 102, 102)),
+            array(new Rgb(255, 133, 51), new Rgb(153, 153, 153)),
+            array(new Rgb(255, 194, 153), new Rgb(204, 204, 204)),
+            array(new Rgb(255, 255, 255), new Rgb(255, 255, 255)),
+            array(new Rgb(255, 0, 0), new Rgb(255, 0, 0)),
+            array(new Rgb(0, 255, 0), new Rgb(0, 255, 0)),
+            array(new Rgb(0, 0, 255), new Rgb(0, 0, 255)),
+        );
+    }
+}
