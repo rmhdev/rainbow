3c4df8217a611cee7d43d8a2b9c27b0967856ee1
rober <rmh.dev@gmail.com>
2015-05-16 00:29:25 +0200

    Convert from hex to rgb

--- src/Rainbow/Converter/HexToRgbConverter.php
+++ src/Rainbow/Converter/HexToRgbConverter.php
@@ -1,17 +1,69 @@
 <?php
 
+/**
+ * This file is part of the Rainbow package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
 namespace Rainbow\Converter;
 
+use Rainbow\Hex;
 use Rainbow\Rgb;
 
 final class HexToRgbConverter implements ConverterInterface
 {
     /**
+     * @var Hex
+     */
+    private $color;
+
+    /**
+     * @var int
+     */
+    private $red;
+
+    /**
+     * @var int
+     */
+    private $green;
+
+    /**
+     * @var int
+     */
+    private $blue;
+
+    /**
+     * @param Hex $color
+     */
+    function __construct(Hex $color)
+    {
+        $this->color = $color;
+    }
+
+    /**
      * {@inheritDoc}
      */
     public function convert()
     {
-        return new Rgb(0, 0, 0);
+        if (!$this->isConverted()) {
+            $this->updateComponents();
+        }
+
+        return new Rgb($this->red, $this->green, $this->blue);
     }
 
+    private function isConverted()
+    {
+        return !is_null($this->red);
+    }
+
+    private function updateComponents()
+    {
+        $this->red = hexdec($this->color->getRed()->getValue());
+        $this->green = hexdec($this->color->getGreen()->getValue());
+        $this->blue = hexdec($this->color->getBlue()->getValue());
+    }
 }
--- tests/Rainbow/Tests/Converter/HexToRgbConverterTest.php
+++ tests/Rainbow/Tests/Converter/HexToRgbConverterTest.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the Rainbow package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
 namespace Rainbow\Tests\Converter;
 
 use Rainbow\Converter\HexToRgbConverter;
@@ -24,6 +32,10 @@ class HexToRgbConverterTest extends \PHPUnit_Framework_TestCase
     {
         return array(
             array(new Rgb(0, 0, 0), new Hex("#000000")),
+            array(new Rgb(255, 0, 0), new Hex("#ff0000")),
+            array(new Rgb(0, 255, 0), new Hex("#00ff00")),
+            array(new Rgb(0, 0, 255), new Hex("#0000ff")),
+            array(new Rgb(50, 150, 250), new Hex("#3296fa")),
         );
     }
 }
